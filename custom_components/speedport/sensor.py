b72cd1f54b1c9b23528d5fcf1b14691a8ce30cd6
gh7531 <huberja1996@gmail.com>
2026-08-23 13:03:40 +0200

    fix(sensor): always create hybrid radio entities, fix dead available() override

--- custom_components/speedport/sensor.py
+++ custom_components/speedport/sensor.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from datetime import datetime
 
 from zoneinfo import ZoneInfo
@@ -20,23 +21,29 @@ from speedport import Speedport
 from custom_components.speedport import DOMAIN
 from custom_components.speedport.device import SpeedportEntity
 
-SENSORS: tuple[SensorEntityDescription, ...] = (
-    SensorEntityDescription(
+
+@dataclass
+class SpeedportSensorEntityDescription(SensorEntityDescription):
+    hybrid_only: bool = False
+
+
+SENSORS: tuple[SpeedportSensorEntityDescription, ...] = (
+    SpeedportSensorEntityDescription(
         key="public_ip_v4",
         name="IPv4",
         icon="mdi:earth",
     ),
-    SensorEntityDescription(
+    SpeedportSensorEntityDescription(
         key="public_ip_v6",
         name="IPv6",
         icon="mdi:earth",
     ),
-    SensorEntityDescription(
+    SpeedportSensorEntityDescription(
         key="inet_uptime",
         name="Internet Uptime",
         device_class=SensorDeviceClass.TIMESTAMP,
     ),
-    SensorEntityDescription(
+    SpeedportSensorEntityDescription(
         key="inet_upload",
         name="Upload",
         state_class=SensorStateClass.MEASUREMENT,
@@ -44,7 +51,7 @@ SENSORS: tuple[SensorEntityDescription, ...] = (
         device_class=SensorDeviceClass.DATA_RATE,
         icon="mdi:upload",
     ),
-    SensorEntityDescription(
+    SpeedportSensorEntityDescription(
         key="inet_download",
         name="Download",
         state_class=SensorStateClass.MEASUREMENT,
@@ -52,47 +59,55 @@ SENSORS: tuple[SensorEntityDescription, ...] = (
         device_class=SensorDeviceClass.DATA_RATE,
         icon="mdi:download",
     ),
-    SensorEntityDescription(
+    SpeedportSensorEntityDescription(
         key="dsl_upstream",
         name="DSL-Link Upstream",
         native_unit_of_measurement=UnitOfDataRate.BITS_PER_SECOND,
         device_class=SensorDeviceClass.DATA_RATE,
         icon="mdi:upload",
     ),
-    SensorEntityDescription(
+    SpeedportSensorEntityDescription(
         key="dsl_downstream",
         name="DSL-Link Downstream",
         native_unit_of_measurement=UnitOfDataRate.BITS_PER_SECOND,
         device_class=SensorDeviceClass.DATA_RATE,
         icon="mdi:download",
     ),
-    SensorEntityDescription(
+    SpeedportSensorEntityDescription(
         key="router_state",
         name="Router State",
         icon="mdi:router-wireless",
     ),
-    SensorEntityDescription(
+    SpeedportSensorEntityDescription(
         key="dsl_pop",
         name="DSL-PoP",
         icon="mdi:map-marker-radius",
     ),
-    SensorEntityDescription(
+    SpeedportSensorEntityDescription(
         key="ex5g_signal_5g",
         name="5G Signal",
         native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
         icon="mdi:signal",
+        hybrid_only=True,
     ),
-    SensorEntityDescription(
-        key="ex5g_freq_5g", name="5G Frequency", icon="mdi:signal-5g"
+    SpeedportSensorEntityDescription(
+        key="ex5g_freq_5g",
+        name="5G Frequency",
+        icon="mdi:signal-5g",
+        hybrid_only=True,
     ),
-    SensorEntityDescription(
+    SpeedportSensorEntityDescription(
         key="ex5g_signal_lte",
         name="LTE Signal",
         native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
         icon="mdi:signal",
+        hybrid_only=True,
     ),
-    SensorEntityDescription(
-        key="ex5g_freq_lte", name="LTE Frequency", icon="mdi:signal-4g"
+    SpeedportSensorEntityDescription(
+        key="ex5g_freq_lte",
+        name="LTE Frequency",
+        icon="mdi:signal-4g",
+        hybrid_only=True,
     ),
 )
 
@@ -102,18 +117,19 @@ async def async_setup_entry(
 ) -> None:
     """Set up entry."""
     speedport: Speedport = hass.data[DOMAIN][entry.entry_id]
+    show_hybrid_sensors = entry.options.get("show_hybrid_sensors", True)
 
     entities = [
         SpeedportBinarySensor(hass, speedport, description)
         for description in SENSORS
-        if speedport.get(description.key) is not None
+        if not description.hybrid_only or show_hybrid_sensors
     ]
 
     async_add_entities(entities)
 
 
 class SpeedportBinarySensor(SpeedportEntity, SensorEntity):
-    entity_description: SensorEntityDescription
+    entity_description: SpeedportSensorEntityDescription
 
     @property
     def native_value(self) -> StateType:
@@ -125,7 +141,9 @@ class SpeedportBinarySensor(SpeedportEntity, SensorEntity):
             return date.replace(tzinfo=ZoneInfo("Europe/Berlin"))
         return data
 
+    @property
     def available(self) -> bool:
-        if self._speedport.get(self.entity_description.key) is None:
-            return False
-        return super().available
+        return (
+            super().available
+            and self._speedport.get(self.entity_description.key) is not None
+        )
